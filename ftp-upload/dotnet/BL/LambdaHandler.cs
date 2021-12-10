803c9ed2c6ac6c5d85ce6a363e3c846f751964d7
Michael Sundgaard <sundgaar@amazon.com>
2021-12-10 14:48:44 +0100

    Cleanup

--- ftp-upload/dotnet/BL/LambdaHandler.cs
+++ ftp-upload/dotnet/BL/LambdaHandler.cs
@@ -27,35 +27,29 @@ namespace LambdaFunction
 
         private void uploadFile()
         {
-            using (var client = new WebClient())
+            var localFilePath = System.Environment.GetEnvironmentVariable("localFilePath");
+            var targetFTPPath = System.Environment.GetEnvironmentVariable("targetFTPPath");
+            var ftpUsername = System.Environment.GetEnvironmentVariable("ftpUsername");
+            var ftpPassword = System.Environment.GetEnvironmentVariable("ftpPassword");
+            if (string.IsNullOrEmpty(localFilePath)) throw new System.Exception("Please define an environment variable for [localFilePath]");
+            if (string.IsNullOrEmpty(targetFTPPath)) throw new System.Exception("Please define an environment variable for [targetFTPPath]");
+            if (string.IsNullOrEmpty(ftpUsername)) throw new System.Exception("Please define an environment variable for [ftpUsername]");
+            if (string.IsNullOrEmpty(ftpPassword)) throw new System.Exception("Please define an environment variable for [ftpPassword]");
+            System.Console.WriteLine($"localFilePath={localFilePath}");
+            System.Console.WriteLine($"targetFTPPath={targetFTPPath}");
+            createLocalDummFile(localFilePath);
+            System.Console.WriteLine($"Local file created.");
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(targetFTPPath);
+            request.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.UsePassive=false;
+            using (Stream fileStream = File.OpenRead(localFilePath))
+            using (Stream ftpStream = request.GetRequestStream())
             {
-                var localFilePath = System.Environment.GetEnvironmentVariable("localFilePath");
-                var targetFTPPath = System.Environment.GetEnvironmentVariable("targetFTPPath");
-                var ftpUsername = System.Environment.GetEnvironmentVariable("ftpUsername");
-                var ftpPassword = System.Environment.GetEnvironmentVariable("ftpPassword");
-                if (string.IsNullOrEmpty(localFilePath)) throw new System.Exception("Please define an environment variable for [localFilePath]");
-                if (string.IsNullOrEmpty(targetFTPPath)) throw new System.Exception("Please define an environment variable for [targetFTPPath]");
-                if (string.IsNullOrEmpty(ftpUsername)) throw new System.Exception("Please define an environment variable for [ftpUsername]");
-                if (string.IsNullOrEmpty(ftpPassword)) throw new System.Exception("Please define an environment variable for [ftpPassword]");
-                System.Console.WriteLine($"localFilePath={localFilePath}");
-                System.Console.WriteLine($"targetFTPPath={targetFTPPath}");
-                client.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
-                createLocalDummFile(localFilePath);
-                System.Console.WriteLine($"Local file created.");
-                //client.UploadFile(new System.Uri(targetFTPPath), WebRequestMethods.Ftp.UploadFile, localFilePath);
-                //client.(targetFTPPath, WebRequestMethods.Ftp.ListDirectory);
-                FtpWebRequest request = (FtpWebRequest)WebRequest.Create(targetFTPPath);
-                request.Credentials = new NetworkCredential(ftpUsername, ftpPassword);
-                request.Method = WebRequestMethods.Ftp.UploadFile;
-                request.Method = WebRequestMethods.Ftp.UploadFile;
-                request.UsePassive=false;
-                using (Stream fileStream = File.OpenRead(localFilePath))
-                using (Stream ftpStream = request.GetRequestStream())
-                {
-                    fileStream.CopyTo(ftpStream);
-                }
-                System.Console.WriteLine($"File uploaded to FTP server.");
+                fileStream.CopyTo(ftpStream);
             }
+            System.Console.WriteLine($"File uploaded to FTP server.");
         }
     }
 }
